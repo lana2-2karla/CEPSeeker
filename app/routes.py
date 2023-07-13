5842758bae9342f9add77257637bb4e54079f383
lana2-2karla <allanakarla1702@gmail.com>
2023-07-13 12:34:21 -0300

    :sparkles: feat: route '/busca' - method GET

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import render_template, request, Blueprint
+from .forms import CEPForm
 import requests
 
-app = Flask(__name__)
+main_blueprint = Blueprint('main', __name__)
 
-app.run()
+@main_blueprint.route('/')
+def index():
+    return render_template('index.html')
+
+@main_blueprint.route('/busca', methods=['GET', 'POST'])
+def busca():
+    cep_form = CEPForm()
+
+    if cep_form.validate_on_submit():
+        cep = cep_form.cep.data
+        response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+        if response.status_code == 200:
+            data = response.json()
+            return render_template('cep_result.html', data=data)
+
+
+    return render_template('busca.html', cep_form=cep_form)
+    
+
+
