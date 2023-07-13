26f541caa6d35af8ef1ed148730b5649981e42cb
lana2-2karla <allanakarla1702@gmail.com>
2023-07-13 16:26:31 -0300

    :ok_hand: style: spacing

--- app/routes.py
+++ app/routes.py
@@ -6,21 +6,18 @@ main_blueprint = Blueprint('main', __name__)
 
 @main_blueprint.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @main_blueprint.route('/busca', methods=['GET', 'POST'])
 def busca():
-    cep_form = CEPForm()
+  cep_form = CEPForm()
 
-    if cep_form.validate_on_submit():
-        cep = cep_form.cep.data
-        response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
-        if response.status_code == 200:
-            data = response.json()
-            return render_template('cep_result.html', data=data)
+  if cep_form.validate_on_submit():
+    cep = cep_form.cep.data
+    response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+    if response.status_code == 200:
+      data = response.json()
+      return render_template('cep_result.html', data=data)
 
-
-    return render_template('busca.html', cep_form=cep_form)
+  return render_template('busca.html', cep_form=cep_form)
     
-
-
