1688bad71ac616bcb6893d6420e9c309a41a2343
lana2-2karla <allanakarla1702@gmail.com>
2023-07-13 16:32:02 -0300

    :sparkles: feat: ip request

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, Blueprint
-from .forms import CEPForm
+from .forms import CEPForm, IPForm
 import requests
 
 main_blueprint = Blueprint('main', __name__)
@@ -11,6 +11,7 @@ def index():
 @main_blueprint.route('/busca', methods=['GET', 'POST'])
 def busca():
   cep_form = CEPForm()
+  ip_form = IPForm()
 
   if cep_form.validate_on_submit():
     cep = cep_form.cep.data
@@ -18,6 +19,13 @@ def busca():
     if response.status_code == 200:
       data = response.json()
       return render_template('cep_result.html', data=data)
+    
+  if ip_form.validate_on_submit():
+    ip = ip_form.ip.data
+    response = requests.get(f'https://ipapi.co/{ip}/json/')
+    if response.status_code == 200:
+      data = response.json()
+      return render_template('ip_result.html', data=data)
 
   return render_template('busca.html', cep_form=cep_form)
     
